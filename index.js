482e2dc298ce00b21327e1c09397679ed3c784e1
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-05 14:56:49 -0700

    Added a get by ID route and create post route for students

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ server.post('/api/cohorts', async (req, res) => {
     }
 }); 
 
+// STRETCH: Adding a new student
+server.post('/api/students', async (req, res) => {
+    try {
+        const [id] = await db('students').insert
+        (req.body); 
+
+        const student = await db('students')
+        .where({ id })
+        .first(); 
+
+        res.status(201).json(student)
+    } catch (error) {
+        const message = errors[error.errno] || "We ran into an error :("; 
+        res.status(500).json({ message, error })
+    }
+})
+
 // GET (R in CRUD)
 server.get("/api/cohorts", async (req, res) => {
   // Get the cohorts from the database
@@ -43,17 +60,6 @@ server.get("/api/cohorts", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
-// Displaying a list of all my students 
-server.get("/api/students", async (req, res) => {
-    // Get the students from the database
-    try {
-      const students = await db("students"); // all the records from the table
-      res.status(200).json(students);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
 
 server.get('/api/cohorts/:id', async (req, res) => {
     // get the cohorts from the database 
@@ -66,6 +72,31 @@ server.get('/api/cohorts/:id', async (req, res) => {
         res.status(500).json(error); 
     }
 }); 
+
+// STRETCH: Displaying a list of all my students 
+server.get("/api/students", async (req, res) => {
+    // Get the students from the database
+    try {
+      const students = await db("students"); // all the records from the table
+      res.status(200).json(students);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+// STRETCH: Displaying students by id
+server.get('/api/students/:id', async (req, res) => {
+    // get the students from the database 
+    try {
+        const student = await db('students')
+        .where({ id: req.params.id })
+        .first(); 
+        res.status(200).json(student);
+    } catch (error) {
+        res.status(500).json(error); 
+    }
+})
+
 
 // Displays all the students within a cohort 
 server.get('/api/cohorts/:id/students', async (req, res) => {
